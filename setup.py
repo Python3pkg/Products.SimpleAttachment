e10404c5a44849c34e8be05c1e3d0eae91c2d418
Maurits van Rees <maurits@vanrees.org>
2016-02-22 17:05:51 +0100

    Cleaned up setup.py.

--- setup.py
+++ setup.py
@@ -2,34 +2,37 @@ from setuptools import setup, find_packages
 
 version = '4.5.dev0'
 
-setup(name='Products.SimpleAttachment',
-      version=version,
-      description="Simple Attachments for Plone",
-      long_description=open("README.md").read() + '\n' +
-            open('CHANGES.txt').read(),
-      classifiers=[
+setup(
+    name='Products.SimpleAttachment',
+    version=version,
+    description="Simple Attachments for Plone",
+    long_description=(open("README.md").read() + '\n' +
+                      open('CHANGES.txt').read()),
+    classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
         "Framework :: Zope2",
         "Programming Language :: Python",
-        ],
-      keywords='Plone attachments RichDocument',
-      author='Martin Aspeli',
-      author_email='',
-      url='http://pypi.python.org/pypi/Products.SimpleAttachment',
-      license='GPL',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['Products'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'archetypes.schemaextender',
-          'plone.app.blob',
-      ],
-      extras_require={ 'test': [
-          'zope.testing',
-          'collective.testcaselayer',
-          'Products.RichDocument',
-      ]},
-      )
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='Plone attachments RichDocument',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='https://github.com/collective/Products.SimpleAttachment',
+    license='GPL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'archetypes.schemaextender',
+        'plone.app.blob',
+    ],
+    extras_require={'test': [
+        'zope.testing',
+        'collective.testcaselayer',
+        'Products.RichDocument',
+    ]},
+)
